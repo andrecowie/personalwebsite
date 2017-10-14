3b3355cd5300991f18bace4e8e99d935ac40648a
Andre Cowie <drecowie@gmail.com>
2017-10-15 12:04:31 +1300

    Dunno aye

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import boto3
 import json
-from flask import Flask, url_for, redirect, session, render_template, request, Response
+from flask import Flask, url_for, redirect, session, render_template, request, Response, send_from_directory
 from passlib.hash import sha256_crypt
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute, BooleanAttribute
@@ -11,16 +11,9 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
-@app.route('/personal', methods=['POST'])
-def personal():
-	print (request.json['answer'])
-	return "Success"
-
-
-@app.route('/whatHereFor', methods=['POST'])
-def whosAndreToRandom():
-	print (request.json['answer'])
-	return "Success"
+@app.route('/charts')
+def chart():
+	return render_template('charts.html')
 
 app.secret_key = "eworuq859r"
 
